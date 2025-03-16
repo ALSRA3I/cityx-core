0bfd101881369b665043c58174004f92747fa59d
Abdulrahman Al Abri <abdulrahman.alabri@student.manchester.ac.uk>
2025-03-16 13:54:58 +0000

    Initial commit

---
+++ src/main/java/com/abdulrahman/cityxcore/CityxCoreApplication.java
@@ -0,0 +1,13 @@
+package com.abdulrahman.cityxcore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CityxCoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CityxCoreApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/abdulrahman/cityxcore/CityxCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abdulrahman.cityxcore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CityxCoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
