798b3fa86e2abdaba501fae150af9a540cd74ca4
Abdulrahman Al Abri <abdulrahman.alabri@student.manchester.ac.uk>
2025-03-16 14:27:36 +0000

    Hello Rihal

--- src/main/java/com/abdulrahman/cityxcore/CityxCoreApplication.java
+++ src/main/java/com/abdulrahman/cityxcore/CityxCoreApplication.java
@@ -2,12 +2,21 @@ package com.abdulrahman.cityxcore;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class CityxCoreApplication {
 
     public static void main(String[] args) {
+
         SpringApplication.run(CityxCoreApplication.class, args);
+    }
+
+    @GetMapping
+    public String helloRihal() {
+        return "Hello Rihal";
     }
 
 }
